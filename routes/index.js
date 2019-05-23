33ad8393f2bdef43e417f4137e0929b284bdaeb2
robert-hanson <handofthesun@gmail.com>
2019-05-23 13:07:04 -0500

    removed dumb comment that was left in

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('do we get here?');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
